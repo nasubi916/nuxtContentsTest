e270369b0f5935b7e0c7de27d4edff0f5eeb4711
nasubi916 <r916nis@gmail.com>
2023-12-11 22:45:41 +0900

    close supabaseで認証 #15

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,9 @@
   <div>
     <!-- ナビゲーションバーを作る -->
     <nav class="flex justify-start">
-      <NuxtLink to="/" class="p-2 border bg-white dark:bg-gray-400 rounded">Home</NuxtLink>
-      <NuxtLink to="/about" class="p-2 border bg-white dark:bg-gray-400 rounded">About</NuxtLink>
-      <NuxtLink to="/dashboard" class="ml-auto mr-3 p-2 border bg-white dark:bg-gray-400 rounded">Dashboard</NuxtLink>
+      <NuxtLink to="/" class="p-2 border bg-blue-400 dark:bg-gray-400 rounded">Home</NuxtLink>
+      <NuxtLink to="/about" class="p-2 border bg-red-400 dark:bg-gray-400 rounded">About</NuxtLink>
+      <NuxtLink to="/dashboard" class="ml-auto mr-3 p-2 border bg-orange-400 dark:bg-gray-400 rounded">Dashboard</NuxtLink>
     </nav>
     <slot />
   </div>
