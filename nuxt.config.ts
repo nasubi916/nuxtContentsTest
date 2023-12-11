996cd6b16f02c69a02f704378cf8a6d3a4d0a1d9
nasubi916 <r916nis@gmail.com>
2023-12-11 22:31:14 +0900

    supabase key 反応してくれ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    serviceKey: process.env.SUPABASE_SERVICE_KEY,
     // redirect: false,
     redirectOptions: {
       login: '/login',
