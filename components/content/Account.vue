ceffbf414194f27d0f00fa77e97c0ae69f62a88a
nasubi916 <r916nis@gmail.com>
2023-12-12 21:36:30 +0900

    CSSの修正

--- components/content/Account.vue
+++ components/content/Account.vue
@@ -11,13 +11,6 @@ const profile = computed(
   () => user?.value.user_metadata.avatar_url
 );
 
-const rootDashboard = async () => {
-  try {
-    await navigateTo('/dashboard');
-  } catch (error) {
-    alert(error.error_description || error.message)
-  }
-};
 const logout = async () => {
   try {
     const { error } = await supabase.auth.signOut();
@@ -30,19 +23,19 @@ const logout = async () => {
 </script>
 
 <template>
-  <div v-if="user" class="rounded p-3 flex flex-row items-center bg-white">
+  <div v-if="user" class="rounded flex flex-row items-center bg-white">
     <button @click="dropBox = !dropBox">
-      <img class="rounded-full w-12 h-12 border-2 border-blue-400" :src="profile" />
+      <img :src="profile" class="rounded-full w-12 h-12 border-2 border-blue-400" />
     </button>
     <div v-if="dropBox"
       class="absolute top-16 right-0 w-48 bg-white rounded-lg shadow-xl border-2 border-b-2 border-gray-600">
       <div class="flex flex-col items-center justify-center">
         <p class="text-lg font-bold text-blue-600 dark:text-gray-400">{{ name }}</p>
         <div class="border-b-2 border-gray-600 w-full"></div>
-        <button @click="rootDashboard(), dropBox = !dropBox"
+        <NuxtLink @click="dropBox = !dropBox" to="/dashboard"
           class="bg-blue-400 hover:bg-blue-500 p-2 items-center w-full h-full">
           <span>Dashboard</span>
-        </button>
+        </NuxtLink>
         <div class="border-b-2 border-gray-600 w-full"></div>
         <button @click="logout(), dropBox = !dropBox"
           class="bg-blue-400 hover:bg-blue-500 p-2 items-center w-full h-full">
@@ -52,8 +45,10 @@ const logout = async () => {
     </div>
   </div>
   <div v-else>
-    <button class="rounded-full w-12 h-12 border-2 border-blue-400">
-      image
-    </button>
+    <NuxtLink to="/login">
+      <div class="rounded-full w-12 h-12 border-2 flex items-center border-blue-400">
+        image
+      </div>
+    </NuxtLink>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,9 @@
 
 <template>
   <div>
-    <nav class="flex justify-start">
-      <NuxtLink to="/" class="p-2 border bg-blue-400 dark:bg-gray-400 rounded">Home</NuxtLink>
-      <NuxtLink to="/about" class="p-2 border bg-red-400 dark:bg-gray-400 rounded">About</NuxtLink>
+    <nav class="flex justify-start m-2">
+      <NuxtLink to="/" class="p-3 border bg-blue-400 dark:bg-gray-400 rounded">Home</NuxtLink>
+      <NuxtLink to="/about" class="p-3 order bg-red-400 dark:bg-gray-400 rounded">About</NuxtLink>
       <Account class="ml-auto mr-2" />
     </nav>
     <slot />
